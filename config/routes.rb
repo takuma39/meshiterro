589e45f92efbb43ec76bee4171488cdd403b502c
vagrant <vagrant@localhost.localdomain>
2020-05-17 08:59:50 +0000

    Add マイページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'users/show'
   devise_for :users
 
   root 'post_images#index'
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
 	resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
+
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
