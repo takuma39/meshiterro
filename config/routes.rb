f543480fdf646fc5392121b2258f883dfbd045b8
vagrant <vagrant@localhost.localdomain>
2020-05-17 05:26:42 +0000

    Update コメント機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show] do
+	resource :post_comments, only: [:create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
